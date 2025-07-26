5280f6aecb3565e6113aedaece0da356cf714c97
AgustinH09 <agus.hernandez9@gmail.com>
2025-07-26 00:52:50 -0300

    Treesitter improvements

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,1 +1,12 @@
 require "nvchad.autocmds"
+
+-- Add any custom autocmds here with vim.defer_fn for non-critical ones
+-- Example:
+-- vim.defer_fn(function()
+--   vim.api.nvim_create_autocmd("FileType", {
+--     pattern = "python",
+--     callback = function()
+--       vim.opt_local.colorcolumn = "88"
+--     end,
+--   })
+-- end, 100)
