b31f4eca414b01b49ab0aa6b0475a3acaf8fd807
AgustinH09 <agus.hernandez9@gmail.com>
2025-11-08 16:28:45 -0300

    [main]: Add minecraft sounds

--- lua/cmds.lua
+++ lua/cmds.lua
@@ -1,14 +1,55 @@
-# BeepBoop Toggle Command
 vim.g.beepboop_active = false
+vim.g.beepboop_profile = vim.g.beepboop_profile or "typewriter"
 
+local plugin_name = "beepboop.nvim"
+
+---
+-- COMMAND 1: :BeepBoop (Toggle)
+---
 vim.api.nvim_create_user_command("BeepBoop", function()
-    if vim.g.beepboop_active then
-        require("lazy").reset("beepboop.nvim")
-        vim.g.beepboop_active = false
-        print("BeepBoop plugin disabled.")
-    else
-        require("lazy").load({ plugins = "beepboop.nvim" })
-        vim.g.beepboop_active = true
-        print("BeepBoop plugin activated.")
-    end
-end, { desc = "Toggle beepboop.nvim (load/unload)" })
+  if vim.g.beepboop_active then
+    require("lazy").reset(plugin_name)
+    vim.g.beepboop_active = false
+    print "BeepBoop plugin disabled."
+  else
+    require("lazy").load { plugins = plugin_name }
+    vim.g.beepboop_active = true
+    print("BeepBoop plugin activated (" .. vim.g.beepboop_profile .. " sounds).")
+  end
+end, {
+  desc = "Toggle BeepBoop plugin (load/unload)",
+  nargs = 0,
+})
+
+---
+-- COMMAND 2: :BeepBoopSelect (Change Profile)
+---
+vim.api.nvim_create_user_command("BeepBoopSelect", function(args)
+  local new_profile = args.fargs[1]
+
+  if not new_profile then
+    print "Error: No profile provided. Use 'typewriter' or 'minecraft'."
+    return
+  end
+
+  if new_profile ~= "typewriter" and new_profile ~= "minecraft" then
+    print "Invalid profile. Choose 'typewriter' or 'minecraft'."
+    return
+  end
+
+  vim.g.beepboop_profile = new_profile
+  print("BeepBoop profile set to: " .. new_profile)
+
+  if vim.g.beepboop_active then
+    print "Reloading BeepBoop with new sounds..."
+
+    require("lazy").reset(plugin_name)
+    require("lazy").load { plugins = plugin_name }
+  end
+end, {
+  desc = "Set BeepBoop sound profile [typewriter|minecraft]",
+  nargs = 1,
+  complete = function(ArgLead, CmdLine, CursorPos)
+    return { "typewriter", "minecraft" }
+  end,
+})
--- lua/plugins/beepboop.lua
+++ lua/plugins/beepboop.lua
@@ -1,9 +1,6 @@
-return {
-  "EggbertFluffle/beepboop.nvim",
-  cmd = "BeepBoop",
-  opts = {
-    audio_player = "paplay",
-    max_sounds = 20,
+local profiles = {
+  typewriter = {
+    sound_directory = vim.fn.stdpath "config" .. "/sounds/typewriter",
     sound_map = {
       {
         auto_command = "InsertCharPre",
@@ -17,7 +14,33 @@ return {
         },
       },
     },
-    -- to nvim user config directory + sounds/typewriter
-    sound_directory = vim.fn.stdpath "config" .. "/sounds/typewriter",
   },
+  minecraft = {
+    sound_directory = vim.fn.stdpath "config" .. "/sounds/minecraft",
+    sound_map = {
+      { trigger_name = "runprogram", key_map = { mode = "i", key_chord = "<BS>" }, sound = "pop.wav" },
+      { auto_command = "VimEnter", sound = "chestopen.wav" },
+      { trigger_name = "chestclosed", auto_command = "VimLeavePre", sound = "chestclosed.wav" },
+      { auto_command = "InsertCharPre", sounds = { "stone1.wav", "stone2.wav", "stone3.wav", "stone4.wav" } },
+      { auto_command = "TextYankPost", sounds = { "hit1.wav", "hit2.wav", "hit3.wav" } },
+      { auto_command = "BufWrite", sounds = { "open_flip1.wav", "open_flip2.wav", "open_flip3.wav" } },
+    },
+  },
+}
+
+return {
+  "EggbertFluffle/beepboop.nvim",
+  name = "beepboop.nvim",
+  lazy = true,
+  opts = function()
+    local profile_name = vim.g.beepboop_profile or "typewriter"
+    local profile_config = profiles[profile_name] or profiles.typewriter
+
+    return {
+      audio_player = "paplay",
+      max_sounds = 20,
+      sound_map = profile_config.sound_map,
+      sound_directory = profile_config.sound_directory,
+    }
+  end,
 }
