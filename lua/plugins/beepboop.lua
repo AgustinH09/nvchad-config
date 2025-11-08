a533208206b1e92b2a65aa097f7e0006b5bd69a2
AgustinH09 <agus.hernandez9@gmail.com>
2025-11-08 15:54:11 -0300

    [main]: Add typewriter sound to use without mech keyboard

--- lua/plugins/beepboop.lua
+++ lua/plugins/beepboop.lua
@@ -4,5 +4,20 @@ return {
   opts = {
     audio_player = "paplay",
     max_sounds = 20,
+    sound_map = {
+      {
+        auto_command = "InsertCharPre",
+        key_map = { mode = "i", key_chord = "<BS>" },
+        sounds = {
+          "sounds_typewriter1.wav",
+          "sounds_typewriter2.wav",
+          "sounds_typewriter3.wav",
+          "sounds_typewriter4.wav",
+          "sounds_typewriter5.wav",
+        },
+      },
+    },
+    -- to nvim user config directory + sounds/typewriter
+    sound_directory = vim.fn.stdpath "config" .. "/sounds/typewriter",
   },
 }
