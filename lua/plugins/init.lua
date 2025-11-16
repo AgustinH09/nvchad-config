f7f0b838b72d292347ae8268a1a6cb828d92bd73
AgustinH09 <agus.hernandez9@gmail.com>
2025-11-16 10:56:06 -0300

    Disable auto update tree focus file & root change

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -49,8 +49,8 @@ return {
         sync_root_with_cwd = true,
         respect_buf_cwd = true,
         update_focused_file = {
-          enable = true,
-          update_root = true,
+          enable = false,
+          update_root = false,
         },
         view = {
           relativenumber = true,
