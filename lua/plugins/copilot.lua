a854d40cd686f7304f8ae76c72fba2945f2a5385
AgustinH09 <agus.hernandez9@gmail.com>
2025-11-21 12:43:46 -0300

    fix node version copilot uses, fixed to 24.3.0 with mise

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -3,10 +3,26 @@ return {
   cmd = "Copilot",
   event = "InsertEnter",
   config = function()
+    local function get_node_path()
+      local versions = { "24.3.0", "stable" }
+      for _, v in ipairs(versions) do
+        local cmd = "mise where node@" .. v
+        local path = vim.trim(vim.fn.system(cmd))
+
+        if vim.v.shell_error == 0 and path ~= "" then
+          local bin = path .. "/bin/node"
+          if vim.fn.executable(bin) == 1 then
+            return bin
+          end
+        end
+      end
+      return vim.fn.exepath "node"
+    end
+
     require("copilot").setup {
       suggestion = { enabled = false },
       panel = { enabled = false },
-      copilot_node_command = vim.fn.exepath "node",
+      copilot_node_command = get_node_path(),
       filetypes = {
         yaml = true,
         markdown = true,
